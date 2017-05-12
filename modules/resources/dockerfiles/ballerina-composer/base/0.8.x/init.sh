b692869d0408784553420c9062861ce7fa2904b4
Amalka Subasinghe <amalkasubasinghe@gmail.com>
2017-05-12 10:57:00 +0530

    ballerina composer 0.8.x docker files

---
+++ modules/resources/dockerfiles/ballerina-composer/base/0.8.x/init.sh
@@ -0,0 +1,4 @@
+sed -i -e 's/api-path-url/http:\/\/'"$API_PATH_URL"'/g' $COMPOSER_HOME/$RUNTIME_NAME/resources/composer/services/workspace-service-config.yaml
+sed -i -e 's/launcher-path-url/ws:\/\/'"$LAUNCHER_PATH_URL"'/g' $COMPOSER_HOME/$RUNTIME_NAME/resources/composer/services/workspace-service-config.yaml
+
+sh $COMPOSER_HOME/$RUNTIME_NAME/bin/composer
