28e7134923816204f9f9b994616848d8a8223d8e
Amalka Subasinghe <amalkasubasinghe@gmail.com>
2017-05-15 19:43:39 +0530

    composer file path

--- modules/resources/dockerfiles/ballerina-composer/base/0.8.x/init.sh
+++ modules/resources/dockerfiles/ballerina-composer/base/0.8.x/init.sh
@@ -1,4 +1,5 @@
 sed -i -e 's/api-path-url/http:\/\/'"$API_PATH_URL"'/g' $COMPOSER_HOME/$RUNTIME_NAME/resources/composer/services/workspace-service-config.yaml
 sed -i -e 's/launcher-path-url/ws:\/\/'"$LAUNCHER_PATH_URL"'/g' $COMPOSER_HOME/$RUNTIME_NAME/resources/composer/services/workspace-service-config.yaml
+sed -i -e 's/root-dir-path/ws:\/\/'"$ROOT_DIR_PATH"'/g' $COMPOSER_HOME/$RUNTIME_NAME/resources/composer/services/workspace-service-config.yaml
 
 sh $COMPOSER_HOME/$RUNTIME_NAME/bin/composer
