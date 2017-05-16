697481489f04a8ca77b19311b87d02180b4150a3
SabraO <sabraosn@gmail.com>
2017-05-16 14:12:09 +0530

    Add debug url to ballerina composer config

--- modules/resources/dockerfiles/ballerina-composer/base/0.8.x/init.sh
+++ modules/resources/dockerfiles/ballerina-composer/base/0.8.x/init.sh
@@ -1,5 +1,6 @@
-sed -i -e 's/api-path-url/http:\/\/'"$API_PATH_URL"'/g' $COMPOSER_HOME/$RUNTIME_NAME/resources/composer/services/workspace-service-config.yaml
-sed -i -e 's/launcher-path-url/ws:\/\/'"$LAUNCHER_PATH_URL"'/g' $COMPOSER_HOME/$RUNTIME_NAME/resources/composer/services/workspace-service-config.yaml
-sed -i -e 's/root-dir-path/ws:\/\/'"$ROOT_DIR_PATH"'/g' $COMPOSER_HOME/$RUNTIME_NAME/resources/composer/services/workspace-service-config.yaml
+sed -i -e 's|api-path-url|http://'"$API_PATH_URL"'|g' $COMPOSER_HOME/$RUNTIME_NAME/resources/composer/services/workspace-service-config.yaml
+sed -i -e 's|launcher-path-url|ws://'"$LAUNCHER_PATH_URL"'|g' $COMPOSER_HOME/$RUNTIME_NAME/resources/composer/services/workspace-service-config.yaml
+sed -i -e 's|debugger-path-url|ws://'"$DEBUGGER_PATH_URL"'|g' $COMPOSER_HOME/$RUNTIME_NAME/resources/composer/services/workspace-service-config.yaml
+sed -i -e 's|root-dir-path|'"$ROOT_DIR_PATH"'|g' $COMPOSER_HOME/$RUNTIME_NAME/resources/composer/services/workspace-service-config.yaml
 
 sh $COMPOSER_HOME/$RUNTIME_NAME/bin/composer
